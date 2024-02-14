3d42e5ce9b7b454be48e143558ea41ca5c3b1463
Balikas <balikas4@gmail.com>
2024-02-14 11:45:54 +0200

    urls, views, html linking update

--- Fashionista/pages/urls.py
+++ Fashionista/pages/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('dashboard/', views.index, name='index'),
+    path('listings/', views.rental_list, name='rental_list'),
+    path('listing/<int:pk>/', views.listing_details, name='listing_details'),
+    path('listing/<int:pk>/done/', views.listing_available, name='is_available'),
 ]
--- Fashionista/pages/views.py
+++ Fashionista/pages/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpRequest, HttpResponse
 from . import models
 
@@ -9,4 +10,23 @@ def index(request: HttpRequest) -> HttpResponse:
         'users_count': models.get_user_model().objects.count(),
     }
     return render(request, 'listings/index.html', context)
+
+def rental_list(request: HttpRequest) -> HttpResponse:
+    return render(request, 'listings/rental_list.html', {
+        'rental_list': models.Listing.objects.all()
+    })
+
+def listing_details(request: HttpRequest, pk: int) -> HttpResponse:
+    return render(request, 'listings/details.html', {
+        'listing': get_object_or_404(models.Listing, pk=pk)
+    })
+
+def listing_available(request: HttpRequest, pk:int) -> HttpResponse:
+    listing = get_object_or_404(models.Listing, pk=pk)
+    listing.is_available = not listing.is_available
+    listing.save()
+    messages.success(request, f"Listing {listing.name} marked as {'done' if listing.is_available else 'undone'}")
+    if request.GET.get('next'):
+        return redirect(request.GET.get('next'))
+    return redirect(rental_list)
 
