5a8f50a204f9777cced9bd83b469a4b1ddbce62f
Balikas <balikas4@gmail.com>
2024-02-15 11:03:18 +0200

    style settings update

--- Fashionista/pages/urls.py
+++ Fashionista/pages/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('dashboard/', views.index, name='index'),
@@ -7,3 +9,6 @@ urlpatterns = [
     path('listing/<int:pk>/', views.listing_details, name='listing_details'),
     path('listing/<int:pk>/done/', views.listing_available, name='is_available'),
 ]
+
+urlpatterns.extend(static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
+urlpatterns.extend(static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
